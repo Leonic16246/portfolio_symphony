8bdc3374b57c9149fbe495995e2fff020f297ee0
Leon <108039973+Leonic16246@users.noreply.github.com>
2026-03-03 23:10:58 +1300

    Referenced pc table in pc entity

--- src/Entity/Pc.php
+++ src/Entity/Pc.php
@@ -3,18 +3,96 @@
 namespace App\Entity;
 
 use App\Repository\PcRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
 
 #[ORM\Entity(repositoryClass: PcRepository::class)]
+#[ORM\Table(name: 'pc')]
 class Pc
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
+    #[ORM\Column(name: 'pc_id', type: Types::BIGINT)]
+    private ?string $pcId = null;
 
-    public function getId(): ?int
+    #[ORM\Column(name: 'user_id', type: 'uuid', nullable: true)]
+    private ?Uuid $userId = null;
+
+    #[ORM\Column(name: 'name', type: Types::TEXT, nullable: true)]
+    private ?string $name = null;
+
+    #[ORM\Column(name: 'cpu', type: Types::TEXT, nullable: true)]
+    private ?string $cpu = null;
+
+    #[ORM\Column(name: 'gpu', type: Types::TEXT, nullable: true)]
+    private ?string $gpu = null;
+
+    #[ORM\Column(name: 'note', type: Types::TEXT, nullable: true)]
+    private ?string $note = null;
+
+    public function getPcId(): ?string
     {
-        return $this->id;
+        return $this->pcId;
+    }
+
+    public function getUserId(): ?Uuid
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(?Uuid $userId): static
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCpu(): ?string
+    {
+        return $this->cpu;
+    }
+
+    public function setCpu(?string $cpu): static
+    {
+        $this->cpu = $cpu;
+
+        return $this;
+    }
+
+    public function getGpu(): ?string
+    {
+        return $this->gpu;
+    }
+
+    public function setGpu(?string $gpu): static
+    {
+        $this->gpu = $gpu;
+
+        return $this;
+    }
+
+    public function getNote(): ?string
+    {
+        return $this->note;
+    }
+
+    public function setNote(?string $note): static
+    {
+        $this->note = $note;
+
+        return $this;
     }
 }
